f016d5e19f911cc3fb8ff4e1d890defa89ae90f4
ty1765 <tyler.potochnik@gmail.com>
2015-10-18 18:16:12 -0400

    in main added file opening

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <iomanip>
 #include <fstream>
+#include <string>
+#include <cstdlib>
 #include "huffman.h"
 
 using namespace std;
@@ -8,7 +10,15 @@ using namespace std;
 int main(int argc, char *argv[])
 {
 	if(argc == 2){
+		string file = argv[1];
+		ifstream inFile;
 
+		inFile.open(file.c_str());
+
+		if(inFile.fail()){
+			cerr << "Failure opening file!" << endl;
+			exit(1);
+		}
 	}
 	else{
 		cerr << "Invalid Number of Arguments!" << endl;
