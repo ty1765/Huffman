fb5d4f1f475e6036649345d104787b38e6c3a9b7
ty1765 <tyler.potochnik@gmail.com>
2015-10-18 20:10:25 -0400

    edited class

--- huffman.h
+++ huffman.h
@@ -1,15 +1,9 @@
 #ifndef HUFFMAN_H
 #define HUFFMAN_H 
 
-#include <queue>
-
 struct huffman {
     int frequency;
     char c; //Character represented in file
-};
-class compareHuffman {
-public:
-    bool operator()(huffman& t1, huffman& t2);   
 };
 
 #endif
